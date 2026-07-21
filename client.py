e84666a7ad4b563c5b66f0e6cc0286e95b8cc78f
omar  hawsawi <omarhaosaoe@gmail.com>
2026-07-21 10:22:03 +0300

    Update client.py

--- client.py
+++ client.py
@@ -4,13 +4,13 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 
-
 class Client:
     """Trains a local copy of the global model on this client's data shard."""
 
     def __init__(self, client_id, dataset):
         self.client_id = client_id
         self.dataset = dataset
+
 
     def train(self, global_model, epochs, batch_size, learning_rate):
         model = copy.deepcopy(global_model)
